7559efa93a4efaad34c302904d6b6c4b6248e1ef
liuziting <liuziting2014@163.com>
2026-01-21 17:04:29 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,10 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    host: '0.0.0.0', // 允许通过 IP 访问
+    port: 5173,      // 你可以根据需要修改端口
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),

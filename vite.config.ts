bc6ec7fbf290b38a0287742c93de3677aaa23063
xiqiuqiu <luxu3883606@gmail.com>
2026-01-25 00:24:33 +0800

    增 esbuild 配置，生产环境去 console 与 debugger。

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,21 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import path from 'path'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   server: {
-    host: '0.0.0.0', // 允许通过 IP 访问
-    port: 5173,      // 你可以根据需要修改端口
+    host: "0.0.0.0", // 允许通过 IP 访问
+    port: 5173, // 你可以根据需要修改端口
   },
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+  esbuild: {
+    // 生产环境移除 console 和 debugger
+    drop: process.env.NODE_ENV === "production" ? ["console", "debugger"] : [],
+  },
+});
